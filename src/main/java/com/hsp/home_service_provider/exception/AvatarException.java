551445ef7ad6bb4668bd04938a5bf9d74a1dc4da
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:04:45 +0330

    Create AvatarException

---
+++ src/main/java/com/hsp/home_service_provider/exception/AvatarException.java
@@ -0,0 +1,7 @@
+package com.hsp.home_service_provider.exception;
+
+public class AvatarException extends HSPException{
+    public AvatarException(String message) {
+        super(message);
+    }
+}
