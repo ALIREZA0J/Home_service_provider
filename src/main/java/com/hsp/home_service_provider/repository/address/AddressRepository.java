a7610e25756ec96bc2745876b11f126e95147158
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:11:53 +0330

    Create AddressRepository

---
+++ src/main/java/com/hsp/home_service_provider/repository/address/AddressRepository.java
@@ -0,0 +1,11 @@
+package com.hsp.home_service_provider.repository.address;
+
+import com.hsp.home_service_provider.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface AddressRepository extends JpaRepository<Address,Long> {
+
+
+}
