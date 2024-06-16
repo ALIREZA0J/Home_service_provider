91bca97842700dde0844f4c040edd6b8aaf26d31
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:51:04 +0330

    Edition in AdminRepository : Add findAdminByGmailAndPassword method

--- src/main/java/com/hsp/home_service_provider/repository/admin/AdminRepository.java
+++ src/main/java/com/hsp/home_service_provider/repository/admin/AdminRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin,Long> {
 
+    Optional<Admin> findAdminByGmailAndPassword(String gmail , String password);
 
 }
