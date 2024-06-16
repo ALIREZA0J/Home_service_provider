d3f17718521bee9f9ea2c375f42f78768a77c628
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:04:04 +0330

    Create SubServiceException

---
+++ src/main/java/com/hsp/home_service_provider/exception/SubServiceException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class SubServiceException extends HSPException{
+
+    public SubServiceException(String message) {
+        super(message);
+    }
+}
