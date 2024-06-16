eeeb57a3e0e8ad51886bfc2981cc9e9571be55eb
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:05:29 +0330

    Create CustomerException

---
+++ src/main/java/com/hsp/home_service_provider/exception/CustomerException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class CustomerException extends HSPException{
+
+    public CustomerException(String message) {
+        super(message);
+    }
+}
