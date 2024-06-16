16d2afa92cb3445d64866fa2056a112ac76c029b
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 19:22:37 +0330

    Create MismatchException

---
+++ src/main/java/com/hsp/home_service_provider/exception/MismatchException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class MismatchException extends HSPException{
+
+    public MismatchException(String message) {
+        super(message);
+    }
+}
