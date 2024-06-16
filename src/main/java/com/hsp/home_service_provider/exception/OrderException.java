5355b47294f4aea17ecfbaee39ba386c6ce5f9a6
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:03:35 +0330

    Create OrderException

---
+++ src/main/java/com/hsp/home_service_provider/exception/OrderException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class OrderException extends HSPException{
+
+    public OrderException(String message) {
+        super(message);
+    }
+}
