d1f4ca047b6097d05d8a38bff4714bf05eb9b3c0
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:50:28 +0330

    Edition in AddressRepository : Add findAddressesByCustomer method

--- src/main/java/com/hsp/home_service_provider/repository/address/AddressRepository.java
+++ src/main/java/com/hsp/home_service_provider/repository/address/AddressRepository.java
@@ -1,11 +1,12 @@
 package com.hsp.home_service_provider.repository.address;
 
 import com.hsp.home_service_provider.model.Address;
+import com.hsp.home_service_provider.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 
 
 public interface AddressRepository extends JpaRepository<Address,Long> {
 
-
+    List<Address> findAddressesByCustomer(Customer customer);
 }
