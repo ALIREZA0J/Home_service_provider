b16d34a2293861cc810d7cb8997ba4979ff9d58e
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 22:08:51 +0330

    Edition MainServiceService : Add register and delete

--- src/main/java/com/hsp/home_service_provider/service/mainservice/MainServiceService.java
+++ src/main/java/com/hsp/home_service_provider/service/mainservice/MainServiceService.java
@@ -1,12 +1,31 @@
 package com.hsp.home_service_provider.service.mainservice;
 
+import com.hsp.home_service_provider.exception.DuplicateException;
+import com.hsp.home_service_provider.exception.NotFoundException;
+import com.hsp.home_service_provider.model.MainService;
 import com.hsp.home_service_provider.repository.mainservice.MainServiceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MainServiceService {
 
     private final MainServiceRepository mainServiceRepository;
+
+
+
+    public MainService register(MainService mainService){
+        if (mainServiceRepository.findMainServiceByServiceName(mainService.getServiceName()).isPresent())
+            throw new DuplicateException("A MainService with name("+mainService.getServiceName()+") is already exist.");
+        return mainServiceRepository.save(mainService);
+    }
+
+    @Transactional
+    public void delete(Long id){
+        MainService mainService = mainServiceRepository
+                .findById(id).orElseThrow(() -> new NotFoundException("Main-Service with id:"+id+" not found."));
+        mainServiceRepository.delete(mainService);
+    }
 }
