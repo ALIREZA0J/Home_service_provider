19583d88e4fb9a9560882b2f0c5ebffa0d3da07b
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:57:06 +0330

    Edition in SubServiceRepository : Add findSubServicesByMainService & findSubServiceByName methods

--- src/main/java/com/hsp/home_service_provider/repository/subservice/SubServiceRepository.java
+++ src/main/java/com/hsp/home_service_provider/repository/subservice/SubServiceRepository.java
@@ -3,13 +3,18 @@ package com.hsp.home_service_provider.repository.subservice;
 
 
 
+import com.hsp.home_service_provider.model.MainService;
 import com.hsp.home_service_provider.model.SubService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
 
 public interface SubServiceRepository extends JpaRepository<SubService,Long> {
 
 
+    List<SubService> findSubServicesByMainService(MainService mainService);
 
+    Optional<SubService> findSubServiceByName(String name);
 
 }
