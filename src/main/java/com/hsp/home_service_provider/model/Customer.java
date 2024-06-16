94cadaf1abc9fdb9cd001f20589f84af1183a609
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 13:52:22 +0330

    Create Customer class

---
+++ src/main/java/com/hsp/home_service_provider/model/Customer.java
@@ -0,0 +1,33 @@
+package com.hsp.home_service_provider.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder(setterPrefix = "set")
+@Table(name = "customer_tbl")
+public class Customer extends Person{
+
+    @Column(name = "credit")
+    private Long credit;
+
+    @OneToMany(mappedBy = "customer" , cascade = CascadeType.ALL)
+    private List<Order> orders;
+
+    @OneToMany(mappedBy = "customer" , cascade = CascadeType.ALL)
+    private List<Address> addresses;
+
+    @OneToMany(mappedBy = "customer" , cascade = CascadeType.ALL)
+    private List<Comment> comments;
+}
