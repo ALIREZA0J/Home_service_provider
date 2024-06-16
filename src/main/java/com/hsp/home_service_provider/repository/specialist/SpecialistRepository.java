51b2e66adb7e0583a2f07b7811cd133c2f4776de
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:19:46 +0330

    Create SpecialistRepository

---
+++ src/main/java/com/hsp/home_service_provider/repository/specialist/SpecialistRepository.java
@@ -0,0 +1,11 @@
+package com.hsp.home_service_provider.repository.specialist;
+
+
+import com.hsp.home_service_provider.model.Specialist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SpecialistRepository extends JpaRepository<Specialist,Long> {
+
+
+}
