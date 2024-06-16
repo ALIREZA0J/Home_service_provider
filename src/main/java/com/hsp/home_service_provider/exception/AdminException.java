2d8156cdd6f4a31d34e94ec5997de7014ad19817
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:04:32 +0330

    Create AdminException

---
+++ src/main/java/com/hsp/home_service_provider/exception/AdminException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class AdminException extends HSPException{
+
+    public AdminException(String message) {
+        super(message);
+    }
+}
