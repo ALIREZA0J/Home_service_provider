363b5060dec4cf8ccd1ab69dc4da8118e81aef76
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:02:09 +0330

    Create ImageInputStreamException

---
+++ src/main/java/com/hsp/home_service_provider/exception/ImageInputStreamException.java
@@ -0,0 +1,7 @@
+package com.hsp.home_service_provider.exception;
+
+public class ImageInputStreamException extends HSPException{
+    public ImageInputStreamException(String message) {
+        super(message);
+    }
+}
