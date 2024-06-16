dcb9e4d8a1c6ec8ed5c57b5b627d188a9d2b1fea
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:52:24 +0330

    Edition in MainServiceRepository : Add findMainServiceByServiceName method

--- src/main/java/com/hsp/home_service_provider/repository/mainservice/MainServiceRepository.java
+++ src/main/java/com/hsp/home_service_provider/repository/mainservice/MainServiceRepository.java
@@ -4,9 +4,9 @@ package com.hsp.home_service_provider.repository.mainservice;
 
 import com.hsp.home_service_provider.model.MainService;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface MainServiceRepository extends JpaRepository<MainService,Long> {
 
+    Optional<MainService> findMainServiceByServiceName(String serviceName);
 }
