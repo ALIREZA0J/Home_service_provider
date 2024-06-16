3ec35fa4329a1d553514fa9bb6257d80a55fbd7d
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:05:16 +0330

    Create CommentException

---
+++ src/main/java/com/hsp/home_service_provider/exception/CommentException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class CommentException extends HSPException{
+
+    public CommentException(String message) {
+        super(message);
+    }
+}
