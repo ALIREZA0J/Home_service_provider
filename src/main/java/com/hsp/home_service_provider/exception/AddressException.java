f2400d3a673f70078ec837ceaa7d3fefe75a90c2
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:04:20 +0330

    Create AddressException

---
+++ src/main/java/com/hsp/home_service_provider/exception/AddressException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class AddressException extends HSPException{
+
+    public AddressException(String message) {
+        super(message);
+    }
+}
