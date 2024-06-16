3bda1555e4d8383ace1aa796b5fe1a1a61560ee5
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:14:49 +0330

    Create AvatarRepository

---
+++ src/main/java/com/hsp/home_service_provider/repository/avatar/AvatarRepository.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.repository.avatar;
+
+
+import com.hsp.home_service_provider.model.Avatar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvatarRepository extends JpaRepository<Avatar,Long> {
+}
