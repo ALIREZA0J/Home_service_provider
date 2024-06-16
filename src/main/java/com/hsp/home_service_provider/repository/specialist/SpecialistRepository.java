5c9d28831e55145677d96adb8b2fedf447074526
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:55:51 +0330

    Edition in SpecialistRepository : Add findSpecialistByGmail , findSpecialistByGmailAndPassword & findSpecialistsBySpecialistStatus methods

--- src/main/java/com/hsp/home_service_provider/repository/specialist/SpecialistRepository.java
+++ src/main/java/com/hsp/home_service_provider/repository/specialist/SpecialistRepository.java
@@ -2,10 +2,17 @@ package com.hsp.home_service_provider.repository.specialist;
 
 
 import com.hsp.home_service_provider.model.Specialist;
+import com.hsp.home_service_provider.model.enums.SpecialistStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
 
 public interface SpecialistRepository extends JpaRepository<Specialist,Long> {
 
+    Optional<Specialist> findSpecialistByGmail(String gmail);
 
+    Optional<Specialist> findSpecialistByGmailAndPassword(String gmail , String password);
+
+    List<Specialist> findSpecialistsBySpecialistStatus(SpecialistStatus specialistStatus);
 }
