68f8532f1ece3e938b82c93d28c1b3e2b7804e9d
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 13:49:46 +0330

    Create Address class

---
+++ src/main/java/com/hsp/home_service_provider/model/Address.java
@@ -0,0 +1,54 @@
+package com.hsp.home_service_provider.model;
+
+import com.hsp.home_service_provider.model.enums.City;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "address_tbl")
+@Builder(setterPrefix = "set")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "city")
+    @Enumerated
+    private City city;
+
+    @Column(name = "street_name")
+    private String street;
+
+    @Column(name = "alley_name")
+    private String alley;
+
+    @Column(name = "plaque_namber")
+    private Integer plaque;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "address",cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    private List<Order> orders;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                " Id- " + id +
+                " | city=" + city +
+                " | street= " + street +
+                " | alley= " + alley +
+                " | plaque= " + plaque +
+                "} " ;
+    }
+}
