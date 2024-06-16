285e92cff0202044d5c77a56d334767b29cd1aec
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:51:38 +0330

    Edition in CustomerRepository : Add findCustomerByGmailAndPassword & findCustomerByGmail methods

--- src/main/java/com/hsp/home_service_provider/repository/customer/CustomerRepository.java
+++ src/main/java/com/hsp/home_service_provider/repository/customer/CustomerRepository.java
@@ -4,11 +4,12 @@ package com.hsp.home_service_provider.repository.customer;
 
 import com.hsp.home_service_provider.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
 
+    Optional<Customer> findCustomerByGmail(String gmail);
 
+    Optional<Customer> findCustomerByGmailAndPassword(String gmail , String password);
 
 }
