5d1fa180ba20906bf7adce7287d0dd9dd0e8b70f
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:20:27 +0330

    Create SubServiceRepository

---
+++ src/main/java/com/hsp/home_service_provider/repository/subservice/SubServiceRepository.java
@@ -0,0 +1,15 @@
+package com.hsp.home_service_provider.repository.subservice;
+
+
+
+
+import com.hsp.home_service_provider.model.SubService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SubServiceRepository extends JpaRepository<SubService,Long> {
+
+
+
+
+}
