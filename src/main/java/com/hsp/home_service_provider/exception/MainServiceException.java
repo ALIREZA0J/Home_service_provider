d04bde8dcce501919f16a33f526a4ff33d79940f
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:02:31 +0330

    Create MainServiceException

---
+++ src/main/java/com/hsp/home_service_provider/exception/MainServiceException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class MainServiceException extends HSPException{
+
+    public MainServiceException(String message) {
+        super(message);
+    }
+}
