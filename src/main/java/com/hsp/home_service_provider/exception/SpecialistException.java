096cbe627a938dc3fb8b759f88e341c12b182de9
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:03:50 +0330

    Create SpecialistException

---
+++ src/main/java/com/hsp/home_service_provider/exception/SpecialistException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class SpecialistException extends HSPException{
+
+    public SpecialistException(String message) {
+        super(message);
+    }
+}
