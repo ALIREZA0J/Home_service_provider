d2b7daec5b4097bff224aa16a06028414a008aa5
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:03:13 +0330

    Create OfferException

---
+++ src/main/java/com/hsp/home_service_provider/exception/OfferException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class OfferException extends HSPException{
+
+    public OfferException(String message) {
+        super(message);
+    }
+}
