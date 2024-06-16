6597ec3e84b9289995bf48e98202655785e5e683
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:01:44 +0330

    Create HSPException

---
+++ src/main/java/com/hsp/home_service_provider/exception/HSPException.java
@@ -0,0 +1,7 @@
+package com.hsp.home_service_provider.exception;
+
+public class HSPException extends RuntimeException {
+    public HSPException(String message) {
+        super(message);
+    }
+}
