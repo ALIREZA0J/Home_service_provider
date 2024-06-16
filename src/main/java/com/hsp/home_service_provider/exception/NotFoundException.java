8e24d881b105de7fac06ad8b6799c44e037db060
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:03:01 +0330

    Create NotFoundException

---
+++ src/main/java/com/hsp/home_service_provider/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class NotFoundException extends HSPException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
