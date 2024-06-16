65861c7395f991eda038cb25d18585e67a7cab4b
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:05:45 +0330

    Create DuplicateException

---
+++ src/main/java/com/hsp/home_service_provider/exception/DuplicateException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class DuplicateException extends HSPException{
+
+    public DuplicateException(String message) {
+        super(message);
+    }
+}
