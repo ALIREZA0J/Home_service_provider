1c78a5f33ac8d793ea06fe9d66873ca7162e7993
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 13:57:19 +0330

    Create OrderStatus enum

---
+++ src/main/java/com/hsp/home_service_provider/model/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package com.hsp.home_service_provider.model.enums;
+
+public enum OrderStatus {
+    WAITING_FOR_THE_SUGGESTION_OF_SPECIALIST,
+    WAITING_FOR_SPECIALIST_SELECTION,
+    WAITING_FOR_THE_SPECIALIST_TO_COME_TO_YOUR_PLACE,
+    STARTED,
+    DONE,
+    PAID
+
+}
