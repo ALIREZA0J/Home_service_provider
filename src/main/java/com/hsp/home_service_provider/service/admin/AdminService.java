0b6a94bc46ca9f1d633e376977df178453db7172
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 19:23:58 +0330

    Edition AdminService : Add logIn and changePassword methods

--- src/main/java/com/hsp/home_service_provider/service/admin/AdminService.java
+++ src/main/java/com/hsp/home_service_provider/service/admin/AdminService.java
@@ -1,5 +1,8 @@
 package com.hsp.home_service_provider.service.admin;
 
+import com.hsp.home_service_provider.exception.AdminException;
+import com.hsp.home_service_provider.exception.MismatchException;
+import com.hsp.home_service_provider.model.Admin;
 import com.hsp.home_service_provider.repository.admin.AdminRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,6 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AdminService {
-
     private final AdminRepository adminRepository;
+
+    public Admin logIn(String gmail , String password){
+        return adminRepository.findAdminByGmailAndPassword(gmail, password)
+                .orElseThrow(() -> new AdminException("Gmail or Password is wrong."));
+    }
+
+    public void changePassword(Admin admin,String password1,String password2){
+        if (!password1.equals(password2))
+            throw new MismatchException("Password and repeat password do not match");
+        admin.setPassword(password1);
+        adminRepository.save(admin);
+    }
+
 }
