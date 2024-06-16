790755856f4f01c1fb4cf59b4606a8590037168e
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:54:36 +0330

    Edition in OrderRepository : Add findOrdersByCustomer , findOrdersByCustomerAndOrderStatus & findOrdersBySubServiceAndOrderStatus methods

--- src/main/java/com/hsp/home_service_provider/repository/order/OrderRepository.java
+++ src/main/java/com/hsp/home_service_provider/repository/order/OrderRepository.java
@@ -3,13 +3,22 @@ package com.hsp.home_service_provider.repository.order;
 
 
 
+import com.hsp.home_service_provider.model.Customer;
 import com.hsp.home_service_provider.model.Order;
+import com.hsp.home_service_provider.model.SubService;
+import com.hsp.home_service_provider.model.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
+    List<Order> findOrdersByCustomer(Customer customer);
 
+    List<Order> findOrdersByCustomerAndOrderStatus(Customer customer, OrderStatus orderStatus);
+
+    List<Order> findOrdersBySubServiceAndOrderStatus(SubService subService, OrderStatus orderStatus);
 
 
 }
