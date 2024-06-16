34ca83c41250d61a1e1a4680eace2a7ff6636374
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-16 14:18:30 +0330

    Create OrderRepository

---
+++ src/main/java/com/hsp/home_service_provider/repository/order/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.hsp.home_service_provider.repository.order;
+
+
+
+
+import com.hsp.home_service_provider.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
+
+
+
+}
