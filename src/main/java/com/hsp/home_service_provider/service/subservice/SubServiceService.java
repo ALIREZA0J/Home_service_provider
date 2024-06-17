790bd1c62fd2b03b867309a4d3793fef3b3c8c0d
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-17 13:20:09 +0330

    Edition SubServiceService : Add delete, findByName, update methods

--- src/main/java/com/hsp/home_service_provider/service/subservice/SubServiceService.java
+++ src/main/java/com/hsp/home_service_provider/service/subservice/SubServiceService.java
@@ -1,10 +1,12 @@
 package com.hsp.home_service_provider.service.subservice;
 
 import com.hsp.home_service_provider.exception.DuplicateException;
+import com.hsp.home_service_provider.exception.NotFoundException;
 import com.hsp.home_service_provider.model.SubService;
 import com.hsp.home_service_provider.repository.subservice.SubServiceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +20,31 @@ public class SubServiceService {
         if (subServiceRepository.findSubServiceByName(subService.getName()).isPresent())
             throw new DuplicateException("Sub-Service with name: " + subService.getName() + " is already exist.");
         return subServiceRepository.save(subService);
+    }
+
+    @Transactional
+    public void delete(String name){
+        SubService subService = findByName(name);
+        subServiceRepository.delete(subService);
+    }
+
+    public SubService findByName(String name){
+        return subServiceRepository.findSubServiceByName(name)
+                .orElseThrow(() -> new NotFoundException("Sub-Service with (name:" + name + ") not found."));
+    }
+
+    public SubService update(SubService subService){
+        SubService subServiceFind = findByName(subService.getName());
+        if (!subServiceFind.getDescription().equals(subService.getDescription())){
+            if (subService.getDescription() != null){
+                if (!subService.getDescription().isBlank())
+                    subServiceFind.setDescription(subService.getDescription());
+            }
+        }
+        if (!subServiceFind.getBasePrice().equals(subService.getBasePrice())){
+            if (subService.getBasePrice() != null)
+                subServiceFind.setBasePrice(subService.getBasePrice());
+        }
+        return subServiceRepository.save(subServiceFind);
     }
 }
