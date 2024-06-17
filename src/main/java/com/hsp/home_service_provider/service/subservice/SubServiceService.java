14f5c7040acb54fd3ef15814541e1d43313aa06b
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-17 11:20:02 +0330

    Edition SubServiceService : Add register method

--- src/main/java/com/hsp/home_service_provider/service/subservice/SubServiceService.java
+++ src/main/java/com/hsp/home_service_provider/service/subservice/SubServiceService.java
@@ -1,5 +1,7 @@
 package com.hsp.home_service_provider.service.subservice;
 
+import com.hsp.home_service_provider.exception.DuplicateException;
+import com.hsp.home_service_provider.model.SubService;
 import com.hsp.home_service_provider.repository.subservice.SubServiceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,12 @@ import org.springframework.stereotype.Service;
 public class SubServiceService {
 
     private final SubServiceRepository subServiceRepository;
+
+
+
+    public SubService register(SubService subService){
+        if (subServiceRepository.findSubServiceByName(subService.getName()).isPresent())
+            throw new DuplicateException("Sub-Service with name: " + subService.getName() + " is already exist.");
+        return subServiceRepository.save(subService);
+    }
 }
