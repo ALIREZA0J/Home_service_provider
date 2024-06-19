b7fbc9852a3d90bc3740f68c3839ac3613a56a28
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-19 14:57:34 +0330

    Create NotValidException

---
+++ src/main/java/com/hsp/home_service_provider/exception/NotValidException.java
@@ -0,0 +1,7 @@
+package com.hsp.home_service_provider.exception;
+
+public class NotValidException extends HSPException{
+    public NotValidException(String message) {
+        super(message);
+    }
+}
