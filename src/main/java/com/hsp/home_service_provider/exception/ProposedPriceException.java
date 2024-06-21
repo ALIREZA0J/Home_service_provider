cc3777678aa1ba4fccd49c632c39bd79bc9d9d35
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-21 11:24:56 +0330

    Add ProposedPriceException

---
+++ src/main/java/com/hsp/home_service_provider/exception/ProposedPriceException.java
@@ -0,0 +1,7 @@
+package com.hsp.home_service_provider.exception;
+
+public class ProposedPriceException extends HSPException{
+    public ProposedPriceException(String message) {
+        super(message);
+    }
+}
