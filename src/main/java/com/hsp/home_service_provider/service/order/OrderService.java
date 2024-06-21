ca21e44582c4ec4238d5a346e93df60296b7b9ce
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-21 11:49:44 +0330

    Edition in OrderService : add register method

--- src/main/java/com/hsp/home_service_provider/service/order/OrderService.java
+++ src/main/java/com/hsp/home_service_provider/service/order/OrderService.java
@@ -1,6 +1,10 @@
 package com.hsp.home_service_provider.service.order;
 
+import com.hsp.home_service_provider.exception.OrderException;
+import com.hsp.home_service_provider.model.Order;
+import com.hsp.home_service_provider.model.enums.OrderStatus;
 import com.hsp.home_service_provider.repository.order.OrderRepository;
+import com.hsp.home_service_provider.utility.Validation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +13,14 @@ import org.springframework.stereotype.Service;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final Validation validation;
+
+    public Order register(Order order){
+        if (validation.checkProposedDateNotBeforeToday(order.getDateOfWork()))
+            throw new OrderException("Date of work most be in future.");
+        validation.checkDescriptionNotBlank(order.getDescription());
+        validation.checkDescriptionPattern(order.getDescription());
+        order.setOrderStatus(OrderStatus.WAITING_FOR_THE_SUGGESTION_OF_SPECIALIST);
+        return orderRepository.save(order);
+    }
 }
