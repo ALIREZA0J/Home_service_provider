4f08d24056ab0500f48ac6d3c1e30e36d5e3102f
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-21 11:24:03 +0330

    Edition in CustomerService : Add orderService, addressService, subServiceService fields // Add registerNewAddress, registerNewOrder

--- src/main/java/com/hsp/home_service_provider/service/customer/CustomerService.java
+++ src/main/java/com/hsp/home_service_provider/service/customer/CustomerService.java
@@ -1,11 +1,14 @@
 package com.hsp.home_service_provider.service.customer;
 
-import com.hsp.home_service_provider.exception.CustomerException;
-import com.hsp.home_service_provider.exception.DuplicateException;
-import com.hsp.home_service_provider.exception.NotFoundException;
-import com.hsp.home_service_provider.exception.NotValidException;
+import com.hsp.home_service_provider.exception.*;
+import com.hsp.home_service_provider.model.Address;
 import com.hsp.home_service_provider.model.Customer;
+import com.hsp.home_service_provider.model.Order;
+import com.hsp.home_service_provider.model.SubService;
 import com.hsp.home_service_provider.repository.customer.CustomerRepository;
+import com.hsp.home_service_provider.service.address.AddressService;
+import com.hsp.home_service_provider.service.order.OrderService;
+import com.hsp.home_service_provider.service.subservice.SubServiceService;
 import com.hsp.home_service_provider.utility.Validation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,9 @@ import java.time.LocalDate;
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final OrderService orderService;
+    private final AddressService addressService;
+    private final SubServiceService subServiceService;
     private final Validation validation;
 
 
@@ -38,5 +44,25 @@ public class CustomerService {
     public Customer findById(Long id){
         return customerRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Customer with (id:" + id + ") not found."));
+    }
+
+    public Address registerNewAddress(Address address, Long customerId){
+        Customer customer = findById(customerId);
+        address.setCustomer(customer);
+        return addressService.register(address);
+    }
+    public Order registerNewOrder(Order order,Long customerId,String subServiceName,Long addressId){
+        Customer customer = findById(customerId);
+        SubService subService = subServiceService.findByName(subServiceName);
+        Address address = addressService.findById(addressId);
+        if (!address.getCustomer().equals(customer))
+            throw new MismatchException("The owner of the address is not the same as the owner of the order");
+        validation.checkProposedPriceNotLessThanSubService(order.getProposedPrice(), subService.getBasePrice());
+        if (order.getProposedPrice() == null)
+            order.setProposedPrice(subService.getBasePrice());
+        order.setCustomer(customer);
+        order.setSubService(subService);
+        order.setAddress(address);
+        return orderService.register(order);
     }
 }
