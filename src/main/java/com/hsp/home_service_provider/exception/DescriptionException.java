e5dcf07dbdc52fe5ea546e998e37cef4ccf3e750
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-21 11:24:30 +0330

    Add DescriptionException

---
+++ src/main/java/com/hsp/home_service_provider/exception/DescriptionException.java
@@ -0,0 +1,7 @@
+package com.hsp.home_service_provider.exception;
+
+public class DescriptionException extends HSPException{
+    public DescriptionException(String message) {
+        super(message);
+    }
+}
