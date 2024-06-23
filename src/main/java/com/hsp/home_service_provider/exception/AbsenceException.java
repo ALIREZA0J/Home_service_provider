5b3667d19518ff52388a113bfd3574cf07d38c3d
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-23 17:11:35 +0330

    Create AbsenceException class

---
+++ src/main/java/com/hsp/home_service_provider/exception/AbsenceException.java
@@ -0,0 +1,7 @@
+package com.hsp.home_service_provider.exception;
+
+public class AbsenceException extends HSPException{
+    public AbsenceException(String message) {
+        super(message);
+    }
+}
