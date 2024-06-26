f9b11ef3eb92af65d2115414dbdd3803d3450038
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-26 18:00:43 +0330

    Add CommentMapper

---
+++ src/main/java/com/hsp/home_service_provider/mapper/CommentMapper.java
@@ -0,0 +1,9 @@
+package com.hsp.home_service_provider.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CommentMapper {
+    CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
+}
