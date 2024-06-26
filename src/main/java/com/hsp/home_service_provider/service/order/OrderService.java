857baf787845cb4de0c5a8e50a9676aa6fd65c94
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-26 12:18:36 +0330

    Edition in OfferService : Add findById, changeStatusOfOrderToWaitingForSpecialistSelection, changeStatusOfOrderToWaitingForSpecialistGoToCustomerPlace, changeStatusOfOrderToStart, changeStatusOfOrderToDone

--- src/main/java/com/hsp/home_service_provider/service/order/OrderService.java
+++ src/main/java/com/hsp/home_service_provider/service/order/OrderService.java
@@ -1,5 +1,6 @@
 package com.hsp.home_service_provider.service.order;
 
+import com.hsp.home_service_provider.exception.NotFoundException;
 import com.hsp.home_service_provider.exception.OrderException;
 import com.hsp.home_service_provider.model.Order;
 import com.hsp.home_service_provider.model.enums.OrderStatus;
@@ -23,4 +24,39 @@ public class OrderService {
         order.setOrderStatus(OrderStatus.WAITING_FOR_THE_SUGGESTION_OF_SPECIALIST);
         return orderRepository.save(order);
     }
+
+    public Order findById(Long id){
+        return orderRepository.findById(id).orElseThrow(() -> new NotFoundException("Order with (id:"+id+") not found."));
+    }
+
+    public void changeStatusOfOrderToWaitingForSpecialistSelection(Long orderId){
+        Order order = findById(orderId);
+        if (order.getOrderStatus().equals(OrderStatus.WAITING_FOR_THE_SUGGESTION_OF_SPECIALIST)) {
+            order.setOrderStatus(OrderStatus.WAITING_FOR_SPECIALIST_SELECTION);
+            orderRepository.save(order);
+        }
+    }
+    public void changeStatusOfOrderToWaitingForSpecialistGoToCustomerPlace(Long orderId){
+        Order order = findById(orderId);
+        if (order.getOrderStatus().equals(OrderStatus.WAITING_FOR_SPECIALIST_SELECTION)) {
+            order.setOrderStatus(OrderStatus.WAITING_FOR_THE_SPECIALIST_TO_COME_TO_YOUR_PLACE);
+            orderRepository.save(order);
+        }
+    }
+    public void changeStatusOfOrderToStart(Long orderId){
+        Order order = findById(orderId);
+        if (order.getOrderStatus().equals(OrderStatus.WAITING_FOR_THE_SPECIALIST_TO_COME_TO_YOUR_PLACE)) {
+            order.setOrderStatus(OrderStatus.STARTED);
+            orderRepository.save(order);
+        }
+    }
+    public void changeStatusOfOrderToDone(Long orderId){
+        Order order = findById(orderId);
+        if (order.getOrderStatus().equals(OrderStatus.STARTED)) {
+            order.setOrderStatus(OrderStatus.DONE);
+            orderRepository.save(order);
+        }
+    }
+
+
 }
