ef0cd2ed4ca09c68b4369dcf49f1750b7878b44f
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-26 17:59:10 +0330

    Add SpecialistController

---
+++ src/main/java/com/hsp/home_service_provider/controller/SpecialistController.java
@@ -0,0 +1,9 @@
+package com.hsp.home_service_provider.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SpecialistController {
+}
