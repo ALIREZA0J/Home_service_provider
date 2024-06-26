e045b301f28ae6de4927b46a5066a336f978ea49
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-26 17:58:43 +0330

    Add AdminController

---
+++ src/main/java/com/hsp/home_service_provider/controller/AdminController.java
@@ -0,0 +1,10 @@
+package com.hsp.home_service_provider.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminController {
+}
