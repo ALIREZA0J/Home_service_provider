dda6fa66c1be277d5c0f2aec1f87712994c509c8
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-26 18:00:06 +0330

    Add AddressMapper

---
+++ src/main/java/com/hsp/home_service_provider/mapper/AddressMapper.java
@@ -0,0 +1,9 @@
+package com.hsp.home_service_provider.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AddressMapper {
+    AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
+}
