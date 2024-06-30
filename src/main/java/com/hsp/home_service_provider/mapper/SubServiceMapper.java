350d5020d0409b58a1695e33dfb9a697b2c43bf4
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-30 20:56:21 +0330

    Edition in SubServiceMapper : add subServiceUpdateRequestToModel method

--- src/main/java/com/hsp/home_service_provider/mapper/SubServiceMapper.java
+++ src/main/java/com/hsp/home_service_provider/mapper/SubServiceMapper.java
@@ -2,6 +2,7 @@ package com.hsp.home_service_provider.mapper;
 
 import com.hsp.home_service_provider.dto.sub_service.SubServiceResponse;
 import com.hsp.home_service_provider.dto.sub_service.SubServiceSaveRequest;
+import com.hsp.home_service_provider.dto.sub_service.SubServiceUpdateRequest;
 import com.hsp.home_service_provider.model.SubService;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -11,4 +12,5 @@ public interface SubServiceMapper {
     SubServiceMapper INSTANCE = Mappers.getMapper(SubServiceMapper.class);
     SubService subServiceSaveRequestToModel(SubServiceSaveRequest request);
     SubServiceResponse subServiceModelToSubServiceResponse(SubService subService);
+    SubService subServiceUpdateRequestToModel(SubServiceUpdateRequest request);
 }
