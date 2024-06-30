c97e630ade4abcfb7780802f0df6aae271ea513c
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-30 17:03:03 +0330

    Edition in AdminController : add @RequestMapping and registerMainService (@Post) method

--- src/main/java/com/hsp/home_service_provider/controller/AdminController.java
+++ src/main/java/com/hsp/home_service_provider/controller/AdminController.java
@@ -1,10 +1,32 @@
 package com.hsp.home_service_provider.controller;
 
 
+import com.hsp.home_service_provider.dto.main_service.MainServiceResponse;
+import com.hsp.home_service_provider.dto.main_service.MainServiceSaveRequest;
+import com.hsp.home_service_provider.mapper.MainServiceMapper;
+import com.hsp.home_service_provider.model.MainService;
+import com.hsp.home_service_provider.service.admin.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
+    private final AdminService adminService;
+
+    @PostMapping("/register-Main_service")
+    public ResponseEntity<MainServiceResponse> registerMainService(@RequestBody MainServiceSaveRequest request){
+        MainService mainService = MainServiceMapper.INSTANCE.mainServiceSaveRequestToModel(request);
+        MainService registerMainService = adminService.registerMainService(mainService);
+        return new ResponseEntity<>
+                (MainServiceMapper.INSTANCE.mainServiceModelToResponse(registerMainService), HttpStatus.CREATED);
+    }
+
+
 }
