7e01ae5feddadbcf5be93eaffd4b3e0c4a3d3247
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-06-30 16:57:24 +0330

    Edition in MainServiceMapper : Add mainServiceSaveRequestToModel, mainServiceModelToResponse Method

--- src/main/java/com/hsp/home_service_provider/mapper/MainServiceMapper.java
+++ src/main/java/com/hsp/home_service_provider/mapper/MainServiceMapper.java
@@ -1,9 +1,16 @@
 package com.hsp.home_service_provider.mapper;
 
+import com.hsp.home_service_provider.dto.main_service.MainServiceResponse;
+import com.hsp.home_service_provider.dto.main_service.MainServiceSaveRequest;
+import com.hsp.home_service_provider.model.MainService;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface MainServiceMapper {
     MainServiceMapper INSTANCE = Mappers.getMapper(MainServiceMapper.class);
+
+    MainService mainServiceSaveRequestToModel(MainServiceSaveRequest request);
+
+    MainServiceResponse mainServiceModelToResponse(MainService mainService);
 }
