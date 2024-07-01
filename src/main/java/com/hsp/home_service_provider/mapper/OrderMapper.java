16662f9b8805b27e4f38a578515c70ab07e10b64
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-01 17:21:26 +0330

    Edition in OrderMapper : add orderSaveRequestToModel and modelToOrderResponse method

--- src/main/java/com/hsp/home_service_provider/mapper/OrderMapper.java
+++ src/main/java/com/hsp/home_service_provider/mapper/OrderMapper.java
@@ -1,9 +1,16 @@
 package com.hsp.home_service_provider.mapper;
 
+import com.hsp.home_service_provider.dto.order.OrderResponse;
+import com.hsp.home_service_provider.dto.order.OrderSaveRequest;
+import com.hsp.home_service_provider.model.Order;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface OrderMapper {
     OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
+
+    Order orderSaveRequestToModel(OrderSaveRequest request);
+
+    OrderResponse modelToOrderResponse(Order order);
 }
