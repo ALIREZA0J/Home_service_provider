30dfa67f944d8c2b497fbf6533edf9e4708fdf85
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-01 11:28:48 +0330

    Edition in CustomerMapper : add customerSaveRequestToModel and customerModelToCustomerResponse methods

--- src/main/java/com/hsp/home_service_provider/mapper/CustomerMapper.java
+++ src/main/java/com/hsp/home_service_provider/mapper/CustomerMapper.java
@@ -1,9 +1,16 @@
 package com.hsp.home_service_provider.mapper;
 
+import com.hsp.home_service_provider.dto.customer.CustomerResponse;
+import com.hsp.home_service_provider.dto.customer.CustomerSaveRequest;
+import com.hsp.home_service_provider.model.Customer;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CustomerMapper {
     CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+
+    Customer customerSaveRequestToModel(CustomerSaveRequest request);
+
+    CustomerResponse customerModelToCustomerResponse(Customer customer);
 }
