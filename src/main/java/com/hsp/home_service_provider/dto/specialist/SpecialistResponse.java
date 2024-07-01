f0d56f9826f80a344913acabe2e568b8231a91c6
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-01 10:31:31 +0330

    Add SpecialistResponse

---
+++ src/main/java/com/hsp/home_service_provider/dto/specialist/SpecialistResponse.java
@@ -0,0 +1,12 @@
+package com.hsp.home_service_provider.dto.specialist;
+
+import com.hsp.home_service_provider.dto.sub_service.SubServiceResponse;
+import com.hsp.home_service_provider.model.enums.SpecialistStatus;
+
+import java.util.Set;
+
+public record SpecialistResponse(Long credit,
+                                 Double score,
+                                 SpecialistStatus specialistStatus,
+                                 Set<SubServiceResponse> subServices) {
+}
