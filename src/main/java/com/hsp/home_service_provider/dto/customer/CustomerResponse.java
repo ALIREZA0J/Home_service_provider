0aa1d12bc78203ddfc90ed39700fac23eca003b9
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-01 11:27:36 +0330

    Add CustomerResponse

---
+++ src/main/java/com/hsp/home_service_provider/dto/customer/CustomerResponse.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.dto.customer;
+
+public record CustomerResponse (String firstName,
+                                String lastName,
+                                String gmail,
+                                Long credit
+){
+}
