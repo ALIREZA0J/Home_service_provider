e6255fb0d8f752ff6d34e9966f71a454706eaa07
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-01 10:32:40 +0330

    Edition in SpecialistMapper : add specialistModelToSpecialistResponse method

--- src/main/java/com/hsp/home_service_provider/mapper/SpecialistMapper.java
+++ src/main/java/com/hsp/home_service_provider/mapper/SpecialistMapper.java
@@ -1,9 +1,13 @@
 package com.hsp.home_service_provider.mapper;
 
+import com.hsp.home_service_provider.dto.specialist.SpecialistResponse;
+import com.hsp.home_service_provider.model.Specialist;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface SpecialistMapper {
     SpecialistMapper INSTANCE = Mappers.getMapper(SpecialistMapper.class);
+
+    SpecialistResponse specialistModelToSpecialistResponse(Specialist specialist);
 }
