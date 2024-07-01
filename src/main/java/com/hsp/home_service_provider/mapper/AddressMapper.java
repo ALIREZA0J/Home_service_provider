edefb69399f85e53189d8eb343ad57cddbf2f594
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-01 12:23:47 +0330

    Edition in AddressMapper : add addressSaveRequestToModel and addressModelToAddressResponse method

--- src/main/java/com/hsp/home_service_provider/mapper/AddressMapper.java
+++ src/main/java/com/hsp/home_service_provider/mapper/AddressMapper.java
@@ -1,9 +1,16 @@
 package com.hsp.home_service_provider.mapper;
 
+import com.hsp.home_service_provider.dto.address.AddressResponse;
+import com.hsp.home_service_provider.dto.address.AddressSaveRequest;
+import com.hsp.home_service_provider.model.Address;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface AddressMapper {
     AddressMapper INSTANCE = Mappers.getMapper(AddressMapper.class);
+
+    Address addressSaveRequestToModel(AddressSaveRequest request);
+
+    AddressResponse addressModelToAddressResponse(Address address);
 }
