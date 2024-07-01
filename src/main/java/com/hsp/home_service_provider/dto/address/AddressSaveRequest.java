4a314bf1de429ae68cd9ddbd0eacc37d296cee82
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-01 12:22:45 +0330

    Create AddressSaveRequest

---
+++ src/main/java/com/hsp/home_service_provider/dto/address/AddressSaveRequest.java
@@ -0,0 +1,11 @@
+package com.hsp.home_service_provider.dto.address;
+
+import com.hsp.home_service_provider.dto.customer.CustomerAddressSaveRequest;
+import com.hsp.home_service_provider.model.enums.City;
+
+public record AddressSaveRequest(City city,
+                                 String street,
+                                 String alley,
+                                 Integer plaque,
+                                 CustomerAddressSaveRequest customer) {
+}
