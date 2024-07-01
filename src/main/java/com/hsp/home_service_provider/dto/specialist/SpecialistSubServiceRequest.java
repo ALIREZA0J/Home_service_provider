3ab5578268a1493971978643827b3e12ef85649f
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-01 10:57:35 +0330

    Add SpecialistSubServiceRequest

---
+++ src/main/java/com/hsp/home_service_provider/dto/specialist/SpecialistSubServiceRequest.java
@@ -0,0 +1,5 @@
+package com.hsp.home_service_provider.dto.specialist;
+
+public record SpecialistSubServiceRequest(String subServiceName,
+                                          String specialistGmail) {
+}
