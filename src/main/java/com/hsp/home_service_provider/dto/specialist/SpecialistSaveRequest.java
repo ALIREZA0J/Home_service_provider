ce5f505e5e17eeafc0d7a8a492a47c4c09116243
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-02 23:00:48 +0330

    Create SpecialistSaveRequest

---
+++ src/main/java/com/hsp/home_service_provider/dto/specialist/SpecialistSaveRequest.java
@@ -0,0 +1,7 @@
+package com.hsp.home_service_provider.dto.specialist;
+
+public record SpecialistSaveRequest(String firstName,
+                                    String lastName,
+                                    String gmail,
+                                    String password) {
+}
