5f1bd761b5cd125a495afac5aaab2fcd57e6299e
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-03 16:31:55 +0330

    Addition in OfferMapper : add offerSaveRequestToModel and offerModelToOfferResponse methods

--- src/main/java/com/hsp/home_service_provider/mapper/OfferMapper.java
+++ src/main/java/com/hsp/home_service_provider/mapper/OfferMapper.java
@@ -1,9 +1,14 @@
 package com.hsp.home_service_provider.mapper;
 
+import com.hsp.home_service_provider.dto.offer.OfferResponse;
+import com.hsp.home_service_provider.dto.offer.OfferSaveRequest;
+import com.hsp.home_service_provider.model.Offer;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface OfferMapper {
     OfferMapper INSTANCE = Mappers.getMapper(OfferMapper.class);
+    Offer offerSaveRequestToModel(OfferSaveRequest request);
+    OfferResponse offerModelToOfferResponse(Offer offer);
 }
