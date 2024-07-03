322256fc7c86af13d8c5af6310614eb5bbcabaeb
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-03 17:15:48 +0330

    Addition in SpecialistController : add offerService field and registerNewOffer (@Post) method

--- src/main/java/com/hsp/home_service_provider/controller/SpecialistController.java
+++ src/main/java/com/hsp/home_service_provider/controller/SpecialistController.java
@@ -1,24 +1,33 @@
 package com.hsp.home_service_provider.controller;
 
+import com.hsp.home_service_provider.dto.offer.OfferResponse;
+import com.hsp.home_service_provider.dto.offer.OfferSaveRequest;
 import com.hsp.home_service_provider.dto.order.OrderResponse;
 import com.hsp.home_service_provider.dto.specialist.SpecialistChangePasswordRequest;
 import com.hsp.home_service_provider.dto.specialist.SpecialistResponse;
 import com.hsp.home_service_provider.dto.specialist.SpecialistSaveRequest;
+import com.hsp.home_service_provider.mapper.OfferMapper;
 import com.hsp.home_service_provider.mapper.OrderMapper;
 import com.hsp.home_service_provider.mapper.SpecialistMapper;
+import com.hsp.home_service_provider.model.Offer;
 import com.hsp.home_service_provider.model.Order;
 import com.hsp.home_service_provider.model.Specialist;
+import com.hsp.home_service_provider.service.offer.OfferService;
 import com.hsp.home_service_provider.service.specialist.SpecialistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/specialist")
 public class SpecialistController {
     private final SpecialistService specialistService;
+    private final OfferService offerService;
 
     @PostMapping("/register")
     public ResponseEntity<SpecialistResponse> register(@RequestBody SpecialistSaveRequest request,
@@ -45,4 +54,10 @@ public class SpecialistController {
         return new ResponseEntity<>(orderResponses,HttpStatus.OK);
     }
 
+    @PostMapping("/register-new-offer")
+    public ResponseEntity<OfferResponse> registerNewOffer(@RequestBody OfferSaveRequest request){
+        Offer offer = OfferMapper.INSTANCE.offerSaveRequestToModel(request);
+        Offer registerOffer = offerService.register(offer);
+        return new ResponseEntity<>(OfferMapper.INSTANCE.offerModelToOfferResponse(registerOffer), HttpStatus.CREATED);
+    }
 }
