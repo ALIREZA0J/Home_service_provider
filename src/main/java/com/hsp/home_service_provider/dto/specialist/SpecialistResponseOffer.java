ab5c11d8a9bab210fdf36cad517465efa86cd412
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-03 16:17:20 +0330

    Create SpecialistResponseOffer

---
+++ src/main/java/com/hsp/home_service_provider/dto/specialist/SpecialistResponseOffer.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.dto.specialist;
+
+public record SpecialistResponseOffer(String firstName,
+                                      String lastName,
+                                      String gmail,
+                                      Long credit,
+                                      Double score) {
+}
