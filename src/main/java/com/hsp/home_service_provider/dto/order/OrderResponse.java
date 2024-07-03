6fba26d10068fbba4abfb5dfe26988b85d4922f9
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-03 16:23:37 +0330

    Amendment in OrderResponse : add OrderStatus, CustomerOrderResponse, SubServiceOrderResponse

--- src/main/java/com/hsp/home_service_provider/dto/order/OrderResponse.java
+++ src/main/java/com/hsp/home_service_provider/dto/order/OrderResponse.java
@@ -1,8 +1,9 @@
 package com.hsp.home_service_provider.dto.order;
 
 import com.hsp.home_service_provider.dto.address.AddressResponse;
-import com.hsp.home_service_provider.dto.customer.CustomerResponse;
-import com.hsp.home_service_provider.dto.sub_service.SubServiceResponse;
+import com.hsp.home_service_provider.dto.customer.CustomerOrderResponse;
+import com.hsp.home_service_provider.dto.sub_service.SubServiceOrderResponse;
+import com.hsp.home_service_provider.model.enums.OrderStatus;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -11,7 +12,8 @@ public record OrderResponse (String description,
                              Long proposedPrice,
                              LocalDate dateOfWork,
                              LocalTime timeOfWork,
-                             CustomerResponse customer,
+                             OrderStatus orderStatus,
+                             CustomerOrderResponse customer,
                              AddressResponse address,
-                             SubServiceResponse subService){
+                             SubServiceOrderResponse subService){
 }
