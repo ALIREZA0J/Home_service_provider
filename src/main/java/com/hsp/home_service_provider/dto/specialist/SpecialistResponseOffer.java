66ef12807ba2c13906094b02e0fb462eb1d80a19
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-03 17:16:16 +0330

    Amendment in SpecialistResponseOffer

--- src/main/java/com/hsp/home_service_provider/dto/specialist/SpecialistResponseOffer.java
+++ src/main/java/com/hsp/home_service_provider/dto/specialist/SpecialistResponseOffer.java
@@ -3,6 +3,5 @@ package com.hsp.home_service_provider.dto.specialist;
 public record SpecialistResponseOffer(String firstName,
                                       String lastName,
                                       String gmail,
-                                      Long credit,
                                       Double score) {
 }
