5c43652950e8f00173871d97fbfcc761b6caba80
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-04 11:46:22 +0330

    Addition in CommentService : add specialistService field and register method

--- src/main/java/com/hsp/home_service_provider/service/comment/CommentService.java
+++ src/main/java/com/hsp/home_service_provider/service/comment/CommentService.java
@@ -1,6 +1,9 @@
 package com.hsp.home_service_provider.service.comment;
 
+import com.hsp.home_service_provider.exception.CommentException;
+import com.hsp.home_service_provider.model.Comment;
 import com.hsp.home_service_provider.repository.comment.CommentRepository;
+import com.hsp.home_service_provider.service.specialist.SpecialistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,12 @@ import org.springframework.stereotype.Service;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final SpecialistService specialistService;
+
+    public Comment register(Comment comment){
+        if (comment.getScore() > 5 || comment.getScore() < 1)
+            throw new CommentException("Score is out of range score should be between (1 - 5)");
+        specialistService.applyCommentScore(comment.getOffer().getSpecialist(), comment.getScore());
+        return commentRepository.save(comment);
+    }
 }
