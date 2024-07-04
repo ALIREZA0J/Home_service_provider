bd8f93cd0c6173f36f1302e1da829bdc3aa3b947
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-04 11:41:58 +0330

    Addition in CommentMapper : add commentSaveRequestToModel, modelToCommentResponse methods

--- src/main/java/com/hsp/home_service_provider/mapper/CommentMapper.java
+++ src/main/java/com/hsp/home_service_provider/mapper/CommentMapper.java
@@ -1,9 +1,15 @@
 package com.hsp.home_service_provider.mapper;
 
+import com.hsp.home_service_provider.dto.comment.CommentResponse;
+import com.hsp.home_service_provider.dto.comment.CommentSaveRequest;
+import com.hsp.home_service_provider.model.Comment;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CommentMapper {
     CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
+
+    Comment commentSaveRequestToModel(CommentSaveRequest request);
+    CommentResponse modelToCommentResponse(Comment comment);
 }
