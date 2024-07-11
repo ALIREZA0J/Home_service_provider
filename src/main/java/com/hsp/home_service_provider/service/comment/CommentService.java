48439bfdf242a70b990f36d01680b5879e74873f
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-12 01:03:01 +0330

    Amendment in CommentService : edit register method

--- src/main/java/com/hsp/home_service_provider/service/comment/CommentService.java
+++ src/main/java/com/hsp/home_service_provider/service/comment/CommentService.java
@@ -4,6 +4,7 @@ import com.hsp.home_service_provider.exception.CommentException;
 import com.hsp.home_service_provider.model.Comment;
 import com.hsp.home_service_provider.repository.comment.CommentRepository;
 import com.hsp.home_service_provider.service.specialist.SpecialistService;
+import com.hsp.home_service_provider.utility.Validation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,12 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final SpecialistService specialistService;
+    private final Validation validation;
 
     public Comment register(Comment comment){
         if (comment.getScore() > 5 || comment.getScore() < 1)
             throw new CommentException("Score is out of range score should be between (1 - 5)");
+        validation.checkDescriptionNotBlank(comment.getMessage());
         specialistService.applyCommentScore(comment.getOffer().getSpecialist(), comment.getScore());
         return commentRepository.save(comment);
     }
