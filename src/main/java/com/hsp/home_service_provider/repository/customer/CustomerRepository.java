23dc3bc70707b1743d4e0caab15077506b22df06
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-11 19:37:37 +0330

    Amendment in CustomerRepository extend class from JpaSpecificationExecutor

--- src/main/java/com/hsp/home_service_provider/repository/customer/CustomerRepository.java
+++ src/main/java/com/hsp/home_service_provider/repository/customer/CustomerRepository.java
@@ -4,9 +4,11 @@ package com.hsp.home_service_provider.repository.customer;
 
 import com.hsp.home_service_provider.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import java.util.Optional;
 
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
+public interface CustomerRepository extends JpaRepository<Customer,Long> , JpaSpecificationExecutor<Customer> {
 
     Optional<Customer> findCustomerByGmail(String gmail);
 
