58a3e9995ebac0ee6492c425e09a6a34cf101d94
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-11 19:38:16 +0330

    Amendment in SpecialistRepository extend interface from JpaSpecificationExecutor

--- src/main/java/com/hsp/home_service_provider/repository/specialist/SpecialistRepository.java
+++ src/main/java/com/hsp/home_service_provider/repository/specialist/SpecialistRepository.java
@@ -4,11 +4,13 @@ package com.hsp.home_service_provider.repository.specialist;
 import com.hsp.home_service_provider.model.Specialist;
 import com.hsp.home_service_provider.model.enums.SpecialistStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import java.util.List;
 import java.util.Optional;
 
 
-public interface SpecialistRepository extends JpaRepository<Specialist,Long> {
+public interface SpecialistRepository extends JpaRepository<Specialist,Long> , JpaSpecificationExecutor<Specialist> {
 
     Optional<Specialist> findSpecialistByGmail(String gmail);
 
