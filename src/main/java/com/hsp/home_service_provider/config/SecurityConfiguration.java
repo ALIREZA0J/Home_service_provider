14ee7f745a20f5945511c78e7788d860856502f9
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-24 17:39:24 +0330

    Create SecurityConfiguration

---
+++ src/main/java/com/hsp/home_service_provider/config/SecurityConfiguration.java
@@ -0,0 +1,70 @@
+package com.hsp.home_service_provider.config;
+
+
+import com.hsp.home_service_provider.service.admin.AdminService;
+import com.hsp.home_service_provider.service.customer.CustomerService;
+import com.hsp.home_service_provider.service.specialist.SpecialistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final CustomerService customerService;
+    private final SpecialistService specialistService;
+    private final AdminService adminService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(s ->
+                        s.requestMatchers("/customer/register").permitAll()
+                                .requestMatchers("/specialist/register").permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/customer/**").hasRole("CUSTOMER")
+                                .requestMatchers("/specialist/**").hasRole("SPECIALIST")
+                                .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(username -> {
+                    try {
+                        return adminService.findByGmail(username);
+                    } catch (Exception ignore) {}
+                    try {
+                        return customerService.findByGmail(username);
+                    } catch (Exception ignore) {}
+                    return specialistService.findByGmail(username);
+                })
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
+
