3ecb370eec16ffd3d8222cb2ddff1057524cffd1
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-25 01:21:09 +0330

    Edit CustomerChangePasswordRequest

--- src/main/java/com/hsp/home_service_provider/dto/customer/CustomerChangePasswordRequest.java
+++ src/main/java/com/hsp/home_service_provider/dto/customer/CustomerChangePasswordRequest.java
@@ -1,7 +1,7 @@
 package com.hsp.home_service_provider.dto.customer;
 
-public record CustomerChangePasswordRequest(String gmail,
-                                            String password,
+public record CustomerChangePasswordRequest(
+                                            String oldPassword,
                                             String newPassword,
                                             String confirmNewPassword) {
 }
