c9a324bffee8afd1ad29a1cc90f7fbe162d368a2
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-25 01:21:30 +0330

    Edit OfferSaveRequest

--- src/main/java/com/hsp/home_service_provider/dto/offer/OfferSaveRequest.java
+++ src/main/java/com/hsp/home_service_provider/dto/offer/OfferSaveRequest.java
@@ -1,13 +1,11 @@
 package com.hsp.home_service_provider.dto.offer;
 
 import com.hsp.home_service_provider.dto.order.OrderRequest;
-import com.hsp.home_service_provider.dto.specialist.SpecialistRequest;
 
 import java.time.LocalTime;
 
 public record OfferSaveRequest(Long offerPrice,
                                Integer daysOfWork,
                                LocalTime durationOfWork,
-                               OrderRequest order,
-                               SpecialistRequest specialist) {
+                               OrderRequest order) {
 }
