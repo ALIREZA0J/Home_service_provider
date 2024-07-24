fa3bbe74fd3d531ba714e2bdbc9035aec5e78615
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-25 01:23:01 +0330

    Edit SpecialistChangePasswordRequest

--- src/main/java/com/hsp/home_service_provider/dto/specialist/SpecialistChangePasswordRequest.java
+++ src/main/java/com/hsp/home_service_provider/dto/specialist/SpecialistChangePasswordRequest.java
@@ -1,7 +1,6 @@
 package com.hsp.home_service_provider.dto.specialist;
 
-public record SpecialistChangePasswordRequest (String gmail,
-                                               String password,
+public record SpecialistChangePasswordRequest (String oldPassword,
                                                String newPassword,
                                                String confirmNewPassword) {
 }
