9a6e548eee9c827e80f823c9aa812120007d3e2a
ALIREZA0J <alireza.jamshdi.khamene@gmail.com>
2024-07-24 23:32:53 +0330

    Create ConfirmationException

---
+++ src/main/java/com/hsp/home_service_provider/exception/ConfirmationException.java
@@ -0,0 +1,8 @@
+package com.hsp.home_service_provider.exception;
+
+public class ConfirmationException extends HSPException {
+
+    public ConfirmationException(String message) {
+        super(message);
+    }
+}
